637aabd508abc869a93b2f989939331c25631e4b
pvlasak <petr.vlasakk@gmail.com>
2026-06-23 06:39:08 +0200

    change of datetime import

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-import datetime
+from datetime import datetime
 
 input_user = input("Please enter your task and deadline as comma separate date. Task and deadline are divided by semi-colon!\n")
 input_list = input_user.split(":")
@@ -8,9 +7,9 @@ print(input_list)
 task = input_list[0]
 deadline = input_list[1].strip()
 
-deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+deadline_date = datetime.strptime(deadline, "%d.%m.%Y")
 print(deadline_date)
-today_date = datetime.datetime.today()
+today_date = datetime.today()
 print(today_date)
 time_remaining = (deadline_date-today_date)
 print(f'Number of hours remaining to meet the deadline: {int(time_remaining.total_seconds() / 60 / 60 )} hours.')
